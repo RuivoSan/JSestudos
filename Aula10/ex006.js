23b19967812efb6551c1e88ae20f0cfb49f68927
Faiizka <zjham14@gmail.com>
2025-12-16 17:43:22 -0300

    documentandoJS006

---
+++ Aula10/ex006.js
@@ -0,0 +1,20 @@
+// Adoro café :)
+// ex006
+/*
+   var a = window.document.getElementById('area')
+        a.addEventListener('click',clicar)
+        a.addEventListener('mouseenter',entrar)
+        a.addEventListener('mouseout',sair)
+
+        function clicar(){    
+            a.innerText = 'Clicou'
+            a.style.background = 'darkgreen'   
+        }
+        function entrar(){
+            a.innerText = 'Dentro'
+        }
+        function sair(){
+            a.innerText = 'Fora'
+            a.style.background = 'green'
+        } 
+*/
---
+++ Aula10/ex007.js
@@ -0,0 +1,13 @@
+//Café
+//exercício 007
+/*
+function somar(){
+            var tn1 = window.document.getElementById('txtn1')
+            var tn2 = window.document.querySelector('input#txtn2')
+            var res = window.document.querySelector('div#res')
+            var n1 = Number(tn1.value)
+            var n2 = Number(tn2.value)
+            var s = n1 + n2
+            res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
+        }
+*/
