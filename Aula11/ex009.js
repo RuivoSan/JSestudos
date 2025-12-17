1c099c6e55c468a1520a45f802763d4cdae041a5
Faiizka <zjham14@gmail.com>
2025-12-17 16:10:55 -0300

    Update ex009.js

--- Aula11/ex009.js
+++ Aula11/ex009.js
@@ -1,6 +1,6 @@
-var pais = 'EUA'
+var pais = 'BR'
 console.log(`Vivendo em ${pais}`)
-if (pais == "Brasil") {
+if (pais == "Brasil" || 'BR') {
     console.log('Você é Brasileiro!')
 }else {
     console.log('Você é Estrangeiro!')
