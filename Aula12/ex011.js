509d4b8b21ce8fdb5cf9189fef0f8ab74043ad1a
Faiizka <zjham14@gmail.com>
2025-12-18 18:08:23 -0300

    Update ex011.js

--- Aula12/ex011.js
+++ Aula12/ex011.js
@@ -1,13 +1,26 @@
-var idade = 17
+
+var idade = 67
 if (idade < 16) {
-    console.log(`com ${idade} anos:`)
-    console.log('Não pode votar')
-}else if (idade < 18) {
-    
-    console.log(`com ${idade} anos:`)
-    console.log('Voto opcional')
+    console.log(`Com ${idade} anos não pode votar.`)
+
+}else if (idade < 18 || (idade > 65)) {    
+    console.log(`Com ${idade} anos o voto é opcional.`)
+}else {
+    console.log(`Com ${idade} anos o voto é obrigatório.`)
+}
+/*
+var idade = 67
+if (idade < 16) {
+    console.log(`Com ${idade} anos não pode votar.`)
+
+}else if (idade < 18) {    
+    console.log(`Com ${idade} anos o voto é opcional.`)
+
+}else if (idade < 67){
+    console.log(`Com ${idade} anos o voto é obrigatório.`)
 
 }else {
-    console.log(`com ${idade} anos:`)
-    console.log('Voto obrigatório')
-}
+    console.log(`Com ${idade} anos o voto volta a ser opcional.`)
+
+} feito por mim por cima da aula, deixei documentado para comparar a ultima parte.
+*/
