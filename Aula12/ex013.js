a5419f5b805f71f5e2f200f15da6330cd942b305
Faiizka <zjham14@gmail.com>
2025-12-18 18:45:11 -0300

    Update ex013.js

--- Aula12/ex013.js
+++ Aula12/ex013.js
@@ -9,19 +9,53 @@ var diaSem = agora.getDay()
     5 = Sexta / Friday
     6 = Sábado / Saturday
 */
-// mostrar dia da semana
+// mostrar dia da semana em if e else
+console.log('Condição Aninhada:')    
 if (diaSem == 0) {
     console.log("Hoje é Domingo | Today is Sunday")
+
 }else if (diaSem == 1) {
     console.log('Hoje é Segunda-feira | Today is Monday')
+
 }else if (diaSem == 2) {
     console.log('Hoje é Terça-feira | Today is Tuesday')
+
 }else if (diaSem == 3) {
     console.log('Hoje é Quarta-feira | Today is Wednesday')
+
 }else if (diaSem == 4) {
     console.log('Hoje é Quinta-feira | Today is Thursday')
+
 }else if (diaSem == 5) {
     console.log('Hoje é Sexta-feira | Today is Friday')
+
 }else {
     console.log('Hoje é Sábado | Today is Saturday')
+
+}
+console.log('')
+console.log('Condição Multipla:')
+// Usando switch
+switch (diaSem) {
+    case 0:
+        console.log("Hoje é Domingo | Today is Sunday")
+        break
+    case 1:
+        console.log('Hoje é Segunda-feira | Today is Monday')
+        break
+    case 2:
+        console.log('Hoje é Terça-feira | Today is Tuesday')
+        break
+    case 3:
+        console.log('Hoje é Quarta-feira | Today is Wednesday')
+        break
+    case 4:
+        console.log('Hoje é Quinta-feira | Today is Thursday')
+        break
+    case 5:
+        console.log('Hoje é Sexta-feira | Today is Friday')
+        break
+    case 6:
+        console.log('Hoje é Sábado | Today is Saturday')
+        break                        
 }
