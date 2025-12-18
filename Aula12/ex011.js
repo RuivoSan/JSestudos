ebb0a5ded0c1a70ee9eb3043ba70aa60a40e51e0
Faiizka <zjham14@gmail.com>
2025-12-18 17:59:56 -0300

    Create ex011.js

---
+++ Aula12/ex011.js
@@ -0,0 +1,13 @@
+var idade = 17
+if (idade < 16) {
+    console.log(`com ${idade} anos:`)
+    console.log('Não pode votar')
+}else if (idade < 18) {
+    
+    console.log(`com ${idade} anos:`)
+    console.log('Voto opcional')
+
+}else {
+    console.log(`com ${idade} anos:`)
+    console.log('Voto obrigatório')
+}
