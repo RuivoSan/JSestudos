57555d7c701bc7792b9cc2bdf5593ebd2cd1e3f0
Faiizka <zjham14@gmail.com>
2025-12-19 16:53:08 -0300

    finalex014

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -1,2 +1,24 @@
-var agora = new Date()
-var hora = agora.getHours()
+function carregar(){
+    var msg = window.document.querySelector('div#msg')
+    var img = window.document.querySelector('img#imagem')
+    var agora = new Date()
+    var hora = agora.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas!`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manhaimg.png'
+        document.body.style.background = '#6bbdf7'
+    }else if (hora <= 18) {
+        img.src = 'tardeimg.png'
+        document.body.style.background = '#bb652fff'
+    }else {
+        img.src = 'noiteimg.png'
+        document.body.style.background = '#303948'
+    }
+
+}
+
+
+
+
