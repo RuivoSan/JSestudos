b995cc3b2db777826a136a66071916fca37a6be9
Faiizka <zjham14@gmail.com>
2025-12-24 08:54:37 -0300

    aulafor

---
+++ Aula14/ambiente.js
@@ -0,0 +1,21 @@
+
+
+/* verificação no ínicio
+var c = 1
+while(c <= 5){
+    console.log(c)
+    c++
+}
+*/
+/* verificação no final
+var c = 1
+do {
+    console.log(c)
+    c++
+}while(c<=5)
+*/
+console.log('Vai começar...')
+for (c=1;c <= 10; c++) {
+    console.log(c)
+}
+console.log('Fim')
