7a924a281c7dc23dd800fc6acfb15c7ccf81c2d7
Faiizka <zjham14@gmail.com>
2025-12-25 17:18:50 -0300

    finalex016

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -4,20 +4,27 @@ function contar(){
     var fpas = document.getElementById('txtpas')
     var res = document.querySelector('div#res')
 
-    res.innerHTML = ``
+    res.innerHTML = ''
 
-    if (txtini.value.length == 0) {
-        window.alert('[Erro] Verifique os valores inseridos e tente novamente')
-        res.innerHTML = `Tente novamente`
+    if (txtini.value.length == 0 || txtfim.value.length == 0 || txtpas.value.length == 0) {
+        res.innerHTML = "Impossível contar"
+        
+    }else if(txtpas.value == 0){
+        window.alert('Passo = 0 Não pode ser aceito! Considerando Passo = 1')
+        var ini = Number(fini.value)
+        var fim = Number(ffim.value)
+        for (var c = ini; c<=fim; c++){
+            res.innerHTML += `${c} &#x1F449`   
+        }
+        res.innerHTML += `&#x2705`
+
     }else {
         var ini = Number(fini.value)
         var fim = Number(ffim.value)
         var pas = Number(fpas.value)
-        for (var c = ini; c<=fim; c=c+pas){
-            res.innerHTML += `${c} `
-            
+        for (c=ini;c<=fim;c=c+pas){
+            res.innerHTML += `${c} &#x1F449`  
         }
-        
-    
+        res.innerHTML += `&#x2705`
     }
 }
