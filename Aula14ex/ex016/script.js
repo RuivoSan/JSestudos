1a91e00aa56d1ee28ea09f600947fdeb592143a8
Faiizka <zjham14@gmail.com>
2025-12-26 18:37:00 -0300

    restaurar

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,30 +1,20 @@
 function contar(){
-    var fini = document.getElementById('txtini')
-    var ffim = document.getElementById('txtfim')
-    var fpas = document.getElementById('txtpas')
-    var res = document.querySelector('div#res')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let pas = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    res.innerHTML = ''
-
-    if (txtini.value.length == 0 || txtfim.value.length == 0 || txtpas.value.length == 0) {
-        res.innerHTML = "Impossível contar"
-        
-    }else if(txtpas.value == 0){
-        window.alert('Passo = 0 Não pode ser aceito! Considerando Passo = 1')
-        var ini = Number(fini.value)
-        var fim = Number(ffim.value)
-        for (var c = ini; c<=fim; c++){
-            res.innerHTML += `${c} &#x1F449`   
-        }
-        res.innerHTML += `&#x2705`
-
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        window.alert('Erro falta numero')
     }else {
-        var ini = Number(fini.value)
-        var fim = Number(ffim.value)
-        var pas = Number(fpas.value)
-        for (c=ini;c<=fim;c=c+pas){
-            res.innerHTML += `${c} &#x1F449`  
+        res.innerHTML = ''
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+
+        
+        for (let c = i; c <= f; c+=p){
+            res.innerHTML += `${c} `
         }
-        res.innerHTML += `&#x2705`
     }
 }
