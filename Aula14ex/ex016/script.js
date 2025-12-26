22654641519b5403a2a400821fe39337f452653f
Faiizka <zjham14@gmail.com>
2025-12-26 19:15:52 -0300

    Update script.js

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -7,15 +7,14 @@ function contar(){
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
         res.innerHTML = 'Impossível contar!'
     }else {
-        res.innerHTML = ''
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(pas.value)
         if (pas.value == 0) {
             window.alert('Passo = 0 é inválido! Considerando Passo = 1.')
             p = 1
-        }
-        
+        } 
         if (i < f) { // Contagem crescente
             for (let c = i; c <= f; c+=p){
                 res.innerHTML += `${c} \u{1F449}`
