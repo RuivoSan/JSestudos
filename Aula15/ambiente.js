dee8a4d6a282a53ea2d429444b805a1902cef5a8
Faiizka <zjham14@gmail.com>
2025-12-28 19:48:48 -0300

    aula15

---
+++ Aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5, 3, 6]
+num.push(7)
+num.sort()
+console.log(`${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro indice tem o valor ${num[0]}`)
+
+for(var c=0;c<=num.length;c++) {
+    console.log(`${num[c]}`)
+}
